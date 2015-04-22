444a40930a927e04f9a0904aad3f9bb2be940cc2
JaHIY <jaklsy@gmail.com>
2015-04-22 17:29:29 +0800

    use URI module

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # 利用海词（dict.cn）查单词的 perl 脚本
 
+## Environment:
+ * Perl 5.10.0+
+ * Required Modules:
+    * [JSON](https://metacpan.org/pod/JSON)
+    * [URI](https://metacpan.org/pod/URI)
+    * if you want the best experience, you should install [Term::ReadLine::Gnu](https://metacpan.org/pod/Term::ReadLine::Gnu).
+
+## How to use:
 ``` bash
 $ ./main.pl apple  
 Define apple
--- main.pl
+++ main.pl
@@ -13,52 +13,57 @@ use Getopt::Long;
 use JSON;
 use LWP::UserAgent;
 use Pod::Usage;
-use Readonly;
 use Term::ANSIColor;
 use Term::ReadLine;
+use URI;
+use URI::QueryParam;
 
-Readonly my $USER_AGENT => q{Mozilla/5.0 (X11; Linux x86_64; rv:33.0) Gecko/20100101 Firefox/33.0};
-Readonly my $DICT_PAGE => q{http://dict.cn/en/search/?q=};
-Readonly my $AJAX_PAGE => q{http://dict.cn/ajax/dictcontent.php};
-Readonly my $DICT_TOKEN => q{dictcn};
-Readonly my $SPACE => q{ };
+my $USER_AGENT = q{Mozilla/5.0 (X11; Linux x86_64; rv:33.0) Gecko/20100101 Firefox/33.0};
+my $DICT_PAGE = q{http://dict.cn/en/search/};
+my $DICT_PAGE_URI = URI->new( $DICT_PAGE );
+my $AJAX_PAGE = q{http://dict.cn/ajax/dictcontent.php};
+my $AJAX_PAGE_URI = URI->new( $AJAX_PAGE );
+my $DICT_TOKEN = q{dictcn};
+my $SPACE = q{ };
+
+sub build_url_with {
+    my ( $uri, $query_params_ref ) = @_;
+    my $uri_clone = $uri->clone;
+    map { $uri_clone->query_param_append( $_, $query_params_ref->{$_} ) } keys %{ $query_params_ref };
+    return $uri_clone;
+}
 
 sub get_json_for_definition_of {
     my ( $word ) = @_;
-    my $word_token = $word.$DICT_TOKEN;
+    my $word_token = $word . $DICT_TOKEN;
     my $user_agent = LWP::UserAgent->new( agent      => $USER_AGENT,
                                           cookie_jar => { hide_cookie2 => 1 },
                                         );
-    my $page_response = $user_agent->get( ${DICT_PAGE}.${word} );
-    if ( $page_response->is_success ) {
-         my ( $dict_pagetoken ) = $page_response->content =~ m{dict_pagetoken="([^"]+)"}xms;
-         if ( defined $dict_pagetoken ) {
-             $word_token .= $dict_pagetoken;
-         }
-         #say $word_token;
-         $user_agent->default_header( ISAJAX => q{yes} );
-         my $ajax_response = $user_agent->post( $AJAX_PAGE,
-                                                { q => $word,
-                                                  s => 5,
-                                                  t => Digest::MD5->new->add($word_token)->hexdigest,
-                                                }
-                                              );
-        #print $ajax_response->request->as_string;
-        #print $ajax_response->headers_as_string;
-        if ( $ajax_response->is_success ) {
-            #say $ajax_response->content;
-            my $dict_hash_ref = JSON->new->utf8->decode($ajax_response->content);
-            return $dict_hash_ref;
-        }
-        else {
-            croak 'Cannot get json from dict.cn!';
-        }
-    }
+    my $page_response = $user_agent->get( build_url_with( $DICT_PAGE_URI, {
+                q => $word,
+            } ) );
+
+    croak 'Cannot get page from dict.cn!' if not $page_response->is_success;
+
+    my ( $dict_pagetoken ) = $page_response->content =~ m{dict_pagetoken="([^"]+)"}xms;
+
+    $word_token .= $dict_pagetoken if defined $dict_pagetoken;
+
+    $user_agent->default_header( ISAJAX => 'yes' );
+    my $ajax_response = $user_agent->post( $AJAX_PAGE_URI, {
+        q => $word,
+        s => 5,
+        t => Digest::MD5->new->add($word_token)->hexdigest,
+    } );
+
+    croak 'Cannot get json from dict.cn!' if not $ajax_response->is_success;
+
+    my $dict_hash_ref = JSON->new->utf8->decode($ajax_response->content);
+    return $dict_hash_ref;
 }
 
 sub color_print {
-    my ( $original_input ) = @_;
-    my $input = encode 'utf-8', $original_input;
+    my ( $input ) = @_;
     pos $input = 0;
     while ( pos $input < length $input ) {
         if ( $input =~ m{\G<i>}gcxms ) {
@@ -88,7 +93,6 @@ sub color_print {
         else {
             print substr($input, pos($input), 1);
             pos $input += 1;
-            #croak 'Cannot match anyting!';
         }
     }
     if ($input !~ m{<br[[:space:]]/>[[:space:]]*\z}xms) {
@@ -132,6 +136,9 @@ sub look_up {
 }
 
 sub main {
+    binmode *STDIN,  ':encoding(utf8)';
+    binmode *STDOUT, ':encoding(utf8)';
+    binmode *STDERR, ':encoding(utf8)';
     my %option_of;
     GetOptions( 'help|h|?' => \$option_of{'help'},
                 'man'      => \$option_of{'man'},
